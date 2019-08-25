c511cdcd64e08262983f15d70510aa2bc6a91b77
ccsmily <370341552@qq.com>
2019-08-25 18:00:41 +0800

    try_catch

--- frontend-test/dist/static/urlconfig.js
+++
@@ -1,1 +0,0 @@
-SERVICE_URL = 'http://localhost:31000'
--- pro-1/mkdist.sh
+++ pro-1/mkdist.sh
@@ -1,8 +1,8 @@
-if test $2 = 'nobuild'
+if test "$2" = "nobuild"
 then
 	echo $2
 else
-	npm run build
+	# npm run build
 	echo 'build'
 rm -rf ../frontend-test/dist/*
 cp -r dist/* ../frontend-test/dist/
